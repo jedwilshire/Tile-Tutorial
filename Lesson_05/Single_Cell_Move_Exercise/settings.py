b06921cb0ee8e11063f5518e70c2727dcae11255
Jed Wilshire <jedwilshire@gmail.com>
2021-09-13 14:28:40 -0500

    solutions to lesson 5 exercise on single tile movement

---
+++ Lesson_05/Single_Cell_Move_Exercise/settings.py
@@ -0,0 +1,16 @@
+### Screen Constants
+TILE_SIZE = 32
+SCREEN_WIDTH = TILE_SIZE * 32
+SCREEN_HEIGHT = TILE_SIZE * 20
+
+
+### Color Constants
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+GREY = (20, 20, 20)
+
+
+### Game Control Constants
+PLAYER_SPEED = 0.4
