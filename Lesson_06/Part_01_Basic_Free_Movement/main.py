4e42edf531448809d9694606a740f1f23f965a12
Jed Wilshire <jedwilshire@gmail.com>
2021-09-13 16:09:52 -0500

    Lesson_06 Basic Movement, Frame_Rate_Ind Movement, Improved Collision Detection

---
+++ Lesson_06/Part_01_Basic_Free_Movement/main.py
@@ -0,0 +1,58 @@
+import pygame, settings, sprites, os
+
+class Application:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
+        self.running = True
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Tile Game Tutorial - Lesson 06 Free Movement and New Collisions')
+        # a group of all the sprites in this game
+        self.sprite_group = pygame.sprite.Group()
+        # a group of all the wall sprites in this game
+        self.wall_group = pygame.sprite.Group() 
+        self.load_walls()
+    
+    def load_walls(self):
+        wall_text = []
+        with open(os.path.join(os.path.dirname(__file__), 'map.txt'), 'r') as f:
+            for line in f:
+                wall_text.append(line.strip())
+        for y, row in enumerate(wall_text):
+            for x, tile in enumerate(row):
+                if tile == '1':
+                    sprites.Wall(self, x, y)
+                # position player in location from map.txt
+                elif tile == 'P':
+                    self.player = sprites.Player(self, x, y)
+            
+    def gameloop(self):
+        while self.running:
+            # currently frame rate affects movement speed, this is bad
+            self.clock.tick(120)
+            ### Process event list ###
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                
+            self.screen.fill(settings.BLACK)
+            self.show_tile_grid()
+            # call sprite update methods
+            self.sprite_group.update()
+            # draw sprites to screen
+            self.sprite_group.draw(self.screen)
+            pygame.display.flip()
+    
+            
+    def show_tile_grid(self):
+        for x in range(0, settings.SCREEN_WIDTH, settings.TILE_SIZE):
+            pygame.draw.line(self.screen, settings.GREY, (x, 0), (x, settings.SCREEN_HEIGHT))
+        for y in range(0, settings.SCREEN_HEIGHT, settings.TILE_SIZE):
+            pygame.draw.line(self.screen, settings.GREY, (0, y), (settings.SCREEN_WIDTH, y))
+
+def main():
+    app = Application()
+    app.gameloop()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
---
+++ Lesson_06/Part_02_Frame_Rate_Independent_Movement/sprites.py
@@ -0,0 +1,62 @@
+import pygame, settings, math
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        super().__init__()
+        # keep refernce to our main.Applicaiton
+        self.game = game
+        # create an image surface of tile size, fill with red
+        self.image = pygame.Surface((settings.TILE_SIZE, settings.TILE_SIZE))
+        self.image.fill(settings.RED)
+        # create rect object for movement/collisions, set position
+        self.rect = self.image.get_rect()
+        self.rect.x = x * settings.TILE_SIZE
+        self.rect.y = y * settings.TILE_SIZE
+        self.vx = 0
+        self.vy = 0
+        # add to the sprite group main.Application
+        self.game.sprite_group.add(self)
+    
+    def check_keys(self):
+        # assume no pressed
+        self.vx, self.vy = 0, 0
+        # get a boolean list of all key states, index is key code
+        # True indicates key is pressed
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.vy = -settings.PLAYER_SPEED
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.vy = settings.PLAYER_SPEED
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.vx = -settings.PLAYER_SPEED
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.vx = settings.PLAYER_SPEED
+        # adjust for frame rate independent move speed
+        self.vx *= self.game.dt
+        self.vy *= self.game.dt
+        # adjust for the diagonal problem
+        if self.vx != 0 and self.vy != 0:
+            self.vx /= math.sqrt(2)
+            self.vy /= math.sqrt(2)
+        
+    
+    # called once per frame
+    def update(self):
+        self.check_keys()
+        self.rect.x += self.vx 
+        self.rect.y += self.vy 
+    
+    
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        super().__init__()
+        self.game = game
+        self.image = pygame.Surface((settings.TILE_SIZE, settings.TILE_SIZE))
+        self.image.fill(settings.BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x * settings.TILE_SIZE
+        self.rect.y = y * settings.TILE_SIZE
+        self.game.wall_group.add(self)
+        self.game.sprite_group.add(self)
+        
